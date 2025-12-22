fa8776148b038acf21932d2bc6f09a6c140fd12c
MagicJinn <jinn.bz@mail.com>
2025-12-23 00:07:39 +0100

    Initial commit

---
+++ src/client/java/magicjinn/serverui/ServerUIDataGenerator.java
@@ -0,0 +1,11 @@
+package magicjinn.serverui;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ServerUIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
